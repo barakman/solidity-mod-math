3ea6b29e3792147487d1cfa2e89dcdb46079a71f
barak manos <>
2023-07-30 11:42:22 +0300

    Improve function `findSquare`

--- project/contracts/ModMath.sol
+++ project/contracts/ModMath.sol
@@ -36,18 +36,20 @@ library ModMath {
     /// @return A square and its 4 roots in the multiplicative group of integers modulo `p * q`
     function findSquare(uint256 p, uint256 q, uint256 start) internal pure returns (Square memory) { unchecked {
         uint256 modulus = _mul(p, q);
-        uint256 pDiv4Ceil = p / 4 + 1;
-        uint256 qDiv4Ceil = q / 4 + 1;
-        uint256 pMulInvQP = _mul(p, _inverse(q, p));
-        uint256 qMulInvPQ = _mul(q, _inverse(p, q));
+        uint256 pDivTwo = p / 2;
+        uint256 qDivTwo = q / 2;
         for (uint256 s = start; s < modulus; s += 1) {
-            if (_gcd(modulus, s) == 1 && _powMod(s, p / 2, p) == 1 && _powMod(s, q / 2, q) == 1) {
-                uint256 sp = _powMod(s, pDiv4Ceil, p);
-                uint256 sq = _powMod(s, qDiv4Ceil, q);
-                uint256 r1 = _mapped(qMulInvPQ, pMulInvQP, 0 + sp, 0 + sq, modulus);
-                uint256 r2 = _mapped(qMulInvPQ, pMulInvQP, 0 + sp, q - sq, modulus);
-                uint256 r3 = _mapped(qMulInvPQ, pMulInvQP, p - sp, 0 + sq, modulus);
-                uint256 r4 = _mapped(qMulInvPQ, pMulInvQP, p - sp, q - sq, modulus);
+            if (_gcd(modulus, s) == 1 && _powMod(s, pDivTwo, p) == 1 && _powMod(s, qDivTwo, q) == 1) {
+                uint256 sp = _powMod(s, p / 4 + 1, p);
+                uint256 sq = _powMod(s, q / 4 + 1, q);
+                uint256 tp = p - sp;
+                uint256 tq = q - sq;
+                uint256 up = _mul(_inverse(p, q), q);
+                uint256 uq = _mul(_inverse(q, p), p);
+                uint256 r1 = _mapped(sp, sq, up, uq, modulus);
+                uint256 r2 = _mapped(sp, tq, up, uq, modulus);
+                uint256 r3 = _mapped(tp, sq, up, uq, modulus);
+                uint256 r4 = _mapped(tp, tq, up, uq, modulus);
                 return Square(s, r1, r2, r3, r4);
             }
         }
@@ -83,11 +85,11 @@ library ModMath {
         return y;
     }}
 
-    function _inverse(uint256 n, uint256 a) private pure returns (uint256) { unchecked {
+    function _inverse(uint256 n, uint256 x) private pure returns (uint256) { unchecked {
         uint256 y1 = 0;
         uint256 y2 = 1;
         uint256 r1 = n;
-        uint256 r2 = a;
+        uint256 r2 = x;
         while (r2 != 0) {
             uint256 t1 = r1 / r2;
             uint256 t2 = addmod(y1, (n - mulmod(t1, y2, n)), n);
