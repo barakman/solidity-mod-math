0d06e97a0e471fa36dabd3bf6c2dcde5b1982f88
barak manos <>
2025-02-13 19:30:30 +0200

    Remove `viaIR: truez`

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,6 @@ module.exports = {
     solidity: {
         version: "0.8.28",
         settings: {
-            viaIR: true,
             optimizer: {
                 enabled: true,
                 runs: 20000
